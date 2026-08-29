a06dd09db4ff4a163282980e013ea44bddc9fb9a
Akshat Panchal <132990890+akshatpanchal18@users.noreply.github.com>
2026-08-29 18:06:07 +0530

    feat: refactor App component and add routing setup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const appRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+]);
+
+export default appRoutes;
